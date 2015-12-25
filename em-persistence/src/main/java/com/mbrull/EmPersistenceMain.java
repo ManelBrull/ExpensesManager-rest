d036dcd41b25cd99b1ec0814ced0f6881f16f627
ManelBrull <mbrull88@gmail.com>
2015-12-25 13:14:01 +0100

    Added main to persistence layer and renamed main of rest layer

---
+++ em-persistence/src/main/java/com/mbrull/EmPersistenceMain.java
@@ -0,0 +1,12 @@
+package com.mbrull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmPersistenceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmPersistenceMain.class, args);
+    }
+}
