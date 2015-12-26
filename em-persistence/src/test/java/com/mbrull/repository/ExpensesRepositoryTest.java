65878f2310fae5ada5dd80e9e881f91c3084b362
ManelBrull <mbrull88@gmail.com>
2015-12-26 20:40:57 +0100

    basic data structure created

---
+++ em-persistence/src/test/java/com/mbrull/repository/ExpensesRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.mbrull.repository;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.mbrull.EmPersistenceMain;
+import com.mbrull.entities.Expense;
+import com.mbrull.test.IntegrationTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource("/test.properties")
+@SpringApplicationConfiguration(classes = EmPersistenceMain.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@DatabaseSetup("/ExpensesData.xml")
+@Category(IntegrationTest.class)
+public class ExpensesRepositoryTest {
+
+    @Autowired
+    private ExpenseRepository repository;
+
+    @Test
+    public void findOne_OneExpenseFound() {
+        Optional<Expense> entry = repository.findOne(1L);
+        assertThat(entry.isPresent(), is(true));
+    }
+
+    @Test
+    public void findOne_OneExpenseNotFound() {
+        Optional<Expense> entry = repository.findOne(100L);
+        assertThat(entry.isPresent(), is(false));
+    }
+
+}
